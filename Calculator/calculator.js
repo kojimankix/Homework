55f5f6dd6a0b6d4574c3338017e1439da6223906
mankix koji <daner0134@gmail.com>
2020-07-16 17:37:05 +0300

    Second commit

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,37 +1,40 @@
 document.addEventListener ("DOMContentLoaded", function(event) {
-function calculate() {
+function calculate(){
     var bill_count = document.getElementById('bill_count').value;
     var service_quality = document.getElementById('service_quality').value;
     var people_num = document.getElementById('people_num').value;
 
-// Simple validation
-if(!bill_count || !service_quality) {
-    alert("Please enter values!");
-    return;
-}
-// Check people input value
-if (!people_num || people_num <= 1) {
-    people_num = 1;
-}
-    else {
-        document.getElementById("each").style.display = "block";
+    //Simple validation
+    if (!bill_count || !service_quality){
+        alert('Please enter values!');
+        return;
     }
 
-// Calculate tips
-var total = (bill_count * service_quality) / people_num;
+    //Check people input value
+    if(!people_num || people_num <=1){
+        people_num=1;
+    }   else {
+        document.getElementById('each').style.display = 'block';
+    }
 
-// Round value
-total = Math.round (total * 100) / 100;
-total = total.toFixed(2);
+    //Calculate tips
+    var total = (bill_count = service_quality) / people_num;
+    
 
-document.getElementById('totalTip').style.display = 'block';
-document.getElementById('tip').innerHTML = total;
+    //Round value
+    total = Math.round(total * 100) / 100;
+    total = total.toFixed(2);
 
-// Hide tips element on load
-document.getElementById('totalTip').style.display = 'none';
-document.getElementById('each').style.display = 'none';
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+
 }
-document.getElementById('calculate').onclick = function () {
+
+//Hide tips element on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+document.getElementById("calculate").onclick = function(){
     calculate();
 };
 });
