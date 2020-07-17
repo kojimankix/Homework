1fa57d1e3f4dbaa4ad9994407bd6dd2fe62baf08
mankix koji <daner0134@gmail.com>
2020-07-17 20:47:28 +0300

    Second commit

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,3 +1,35 @@
+var p = document.getElementById("list") ;
+var length = 1 ;
 function add_list() {
-       
+    var a = document.getElementById ("field").value;
+    if (!a){
+        alert ("You have not entered anything!");
+    }
+    else{
+        if (length<=7){
+    var newlist = document.createElement("div") ;
+    newlist.className = "list1"
+    newlist.name = "newlist" ;
+    var div = document.createElement('div') ;
+    if (a.length < 18){
+    div.innerHTML = length + ") " + a;
+    }
+    else{
+        alert ("Too many characters!");
+    }
+    div.appendChild(newlist);
+    p.appendChild(div);
+    p.style.display = "block";
+    length++ ;
+    }
+    else{
+        alert ("No place!")
+    }
+    document.getElementById("field").value = "";
+}
+}
+
+function delete_list(){
+    document.getElementById("list").remove();
+    length = 1;
 }
