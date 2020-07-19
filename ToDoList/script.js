a8aef18e2039fcbe873e720e99aa4abaff758ee8
mankix koji <daner0134@gmail.com>
2020-07-19 18:54:39 +0300

    Second commit

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,35 +1,47 @@
-var p = document.getElementById("list") ;
-var length = 1 ;
-function add_list() {
-    var a = document.getElementById ("field").value;
-    if (!a){
-        alert ("You have not entered anything!");
-    }
-    else{
-        if (length<=7){
-    var newlist = document.createElement("div") ;
-    newlist.className = "list1"
-    newlist.name = "newlist" ;
-    var div = document.createElement('div') ;
-    if (a.length < 18){
-    div.innerHTML = length + ") " + a;
-    }
-    else{
-        alert ("Too many characters!");
-    }
-    div.appendChild(newlist);
-    p.appendChild(div);
-    p.style.display = "block";
-    length++ ;
-    }
-    else{
-        alert ("No place!")
-    }
-    document.getElementById("field").value = "";
-}
-}
+/*
+var input = document.querySelector("input[type='text']");
+var lists = document.querySelectorAll('li');
 
-function delete_list(){
-    document.getElementById("list").remove();
-    length = 1;
-}
+var tipsBtn = document.querySelector('.tipBtn');
+
+var closeBtn = document.getElementById('overlay');
+
+
+tipsBtn.addEventListener("click", function() {
+    overlay.style.height = "100%";
+});
+
+closeBtn.addEventListener("click", function() {
+    exports.preventDefault();
+    overlay.style.height = "0";
+}*/
+var length = 1 ;
+    $(document).ready(function(){      
+        $("#addlist").click(function(){
+            a = document.getElementById ("field").value;
+            if (!a){
+                alert ("You have not entered anything!");
+            }
+            else{
+                    if (a.length < 18){
+                $("ul").append("<li>"+ a +"</li>");
+                length++ ;
+                }
+                else{
+                    alert ("Too many characters!");
+                }
+                document.getElementById("field").value = "";
+            }
+        });
+
+        $("#deletelist").click(function(){
+            $("li").remove();
+            length = 1;
+        });
+        $("#deleteonelist").click(function(){
+            if (length >= 2){
+            $("li:last-child").remove();
+            length-- ;
+            }
+        });
+      }); 
